8de17a0bca609ed3eb03a9b2d64c1269606eef22
unknown <yaserbagheri22@example.com>
2026-07-17 16:59:26 -0700

    fix: add base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  base: '/birthday-countdown/', // این خط برای حل مشکل صفحه سفید اضافه شد
 })
